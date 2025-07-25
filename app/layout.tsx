3163619b763c1f7dad18d4cd53f6b85bfad8b8ea
Mkn0021 <nayeem21122002@gmail.com>
2025-07-25 23:26:21 +0600

    Add Clark for Authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ClerkProvider } from '@clerk/nextjs'
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,11 +24,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className="bg-white text-gray-900">
+        <ClerkProvider>
+          {children}
+        </ClerkProvider>
       </body>
     </html>
   );
