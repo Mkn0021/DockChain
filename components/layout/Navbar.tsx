901e479e5579fde62a9ab22dbc89ec236703d518
Mkn0021 <nayeem21122002@gmail.com>
2025-07-31 02:51:21 +0600

    refactor(Navbar): Introduce NavbarProps interface and replace static buttons with children prop

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import Logo from '../ui/Logo';
-import { Button } from '../ui/Button';
 
-export const Navbar = () => {
+interface NavbarProps {
+    children?: React.ReactNode;
+}
+
+export const Navbar: React.FC<NavbarProps> = ({ children }) => {
     return (
-        <div className="m-0 text-center flex justify-between items-center w-full px-6 py-8 md:px-12 lg:px-20 shadow-none h-[100px] relative">
+        <div className="dark m-0 text-center flex justify-between items-center w-full px-6 py-8 md:px-12 lg:px-20 shadow-none h-[100px] relative">
             <Logo />
             <div className="hidden md:flex gap-4">
-                <Button variant="secondary" href="#how-it-works">
-                    How It Works?
-                </Button>
-                <Button variant="primary" href="/login">
-                    Login
-                </Button>
+                {children}
             </div>
         </div>
     );
