18d98fafe9ce856e1e6acdf176eb4ddaccb36b4b
Mkn0021 <nayeem21122002@gmail.com>
2025-10-19 01:54:56 +0600

    feat(blockchain): add Jest testing framework and initial tests for BlockchainDocumentService
    - Updated package.json to include Jest and related dependencies.
    - Configured TypeScript to recognize Jest types.
    - Created jest.config.js for Jest configuration.

---
+++ blockchain/jest.config.js
@@ -0,0 +1,19 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'js'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      tsconfig: 'tsconfig.json'
+    }]
+  },
+  testMatch: [
+    '**/*.test.ts'
+  ],
+  globals: {
+    'ts-jest': {
+      isolatedModules: true
+    }
+  }
+};
