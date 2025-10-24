faf980b2bec0e9f7e2f1480fcf4101b093436936
Mkn0021 <nayeem21122002@gmail.com>
2025-10-24 11:53:49 +0600

    feat(frontend/auth): implement AuthService for user authentication and authorization handling

---
+++ frontend/types/auth.type.ts
@@ -0,0 +1,37 @@
+export interface FormData {
+    name: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+    otp: string;
+}
+
+export interface RegisterPayload {
+    name: string;
+    email: string;
+    password: string;
+}
+
+export interface LoginPayload {
+    email: string;
+    password: string;
+}
+
+export interface VerifyEmailPayload {
+    email: string;
+    otp: string;
+}
+
+export interface ForgotPasswordPayload {
+    email: string;
+}
+
+export interface LoginResponse {
+    id: string;
+    name: string;
+    email: string;
+    role: string;
+    isVerified: boolean;
+    accessToken: string;
+    refreshToken: string;
+}
