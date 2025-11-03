5c55d37ed81e9840a3ce69138f589ae7c1ec66ce
Mkn0021 <nayeem21122002@gmail.com>
2025-11-03 18:56:12 +0600

    feat(frontend/tailwind): add new paths for data and utils directories in Tailwind config

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -6,6 +6,8 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './lib/**/*.{js,ts,jsx,tsx,mdx}',
+    './data/**/*.{js,ts,jsx,tsx,mdx}',
+    './utils/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
